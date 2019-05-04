3e2c8133a079bba82ee9a46fced67c4fb5d0bf5c
Danilo Domingos <danilo@ubuntu.lan>
2019-05-04 12:24:22 -0300

    Sequencia de metodos.

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,23 @@
 import {Observable} from 'rxjs/Observable';
 
 let observable = Observable.create((observer: any)=> {
-  observer.next('Hey guys!');
+  
+  try {
+
+    observer.next('Hey guys!');
+    observer.next('How are you?');
+    observer.complete();
+    observer.next('This will not send!');
+  }
+  catch (err) {
+    observer.error(err);
+  }
 });
 
-observable.subscribe((x: any) => addItem(x));
+observable.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem('Completed'));
 
 function addItem(val: any) {
   let node = document.createElement('li');
